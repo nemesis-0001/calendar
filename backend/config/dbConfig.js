34814f1c0f314b8d7f080dc8ff80c8ce807b6f67
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-12-09 08:57:11 +0530

    Update dbConfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,20 +1,32 @@
-const mysql = require('mysql2');
+import { createConnection } from "mysql2";
 require("dotenv").config();
 
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database:process.env.DB_NAME
+// });
 
-const connection = mysql.createConnection({
+const connection = createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database:process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+// connection
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.stack);
+    console.error("Error connecting to the database:", err.stack);
     return;
   }
-  console.log('Connected to the database as id ' + connection.threadId);
+  console.log("Connected to the database as id " + connection.threadId);
 });
-  
-module.exports = connection;
+
+// module.exports = pool.promise(); // For using async/await
+export default connection.promise();
