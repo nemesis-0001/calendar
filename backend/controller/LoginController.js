ed731fd28c0fc277daa397128f85a0b2a129b32d
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-12-09 09:42:08 +0530

    Update LoginController.js

--- backend/controller/LoginController.js
+++ backend/controller/LoginController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/dbConfig");
+const db = require("../config/dbConfig").default;
 
 const Register = async (req, res) => {
   const { email, uid } = req.body;
